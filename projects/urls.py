519de619ddb3c617047f337f2e154959d78b3b13
lawrence <lawt90@gmail.com>
2023-12-19 10:28:40 +0200

    Added home page & project section

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_list, name='project_list'),
+    path('<int:pk>/', views.project_list, name='project_list'),
     path('<int:pk>/', views.project_detail, name='project_detail'),
+    path('', views.home, name='home'),
+    path('projects/', views.project_list, name='projects'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,12 @@ def project_list(request):
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     return render(request, 'projects/project_detail.html', {'project': project})
+
+def home(request):
+    name = "John Doe"
+    project_categories = ['Automation', 'Web App Development', 'Backend']
+    context = {
+        'name': name,
+        'project_categories': project_categories,
+    }
+    return render(request, 'projects/home.html', context)
